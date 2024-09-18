6a88637a561d5c499e21197f774d43e79b09ab4c
chandlersong <chandler605@gmail.com>
2024-09-18 15:31:18 +0800

    add a websocket example

---
+++ websocket_ex/tests/helloworld_tungstenite.rs
@@ -0,0 +1,22 @@
+#[cfg(test)]
+mod tests {
+    use tungstenite::{connect, Message};
+
+    #[test]
+    fn test_hello() {
+        let (mut socket, response) = connect("wss://toolin.cn/echo").expect("Can't connect");
+
+        println!("Connected to the server");
+        println!("Response HTTP code: {}", response.status());
+        println!("Response contains the following headers:");
+        for (header, _value) in response.headers() {
+            println!("* {header}");
+        }
+
+        socket.send(Message::Text("Hello WebSocket".into())).unwrap();
+        loop {
+            let msg = socket.read().expect("Error reading message");
+            println!("Received: {msg}");
+        }
+    }
+}
