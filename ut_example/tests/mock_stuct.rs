1399448d95377b554d1835e2c827030583c36602
chandlersong <chandler605@gmail.com>
2025-01-20 15:50:17 +0800

    add mock example

--- ut_example/tests/mock_stuct.rs
+++ ut_example/tests/mock_stuct.rs
@@ -9,13 +9,27 @@ mod thing {
             1
         }
     }
+
+    pub struct AsyncThing {}
+
+    #[automock]
+    impl AsyncThing {
+        pub async fn foo(&self) -> u32 {
+            1
+        }
+    }
 }
 
 #[double]
+use thing::AsyncThing;
+#[double]
 use thing::Thing;
-
 fn do_stuff(thing: &Thing) -> u32 {
     thing.foo()
+}
+
+async fn do_async_stuff(thing: &AsyncThing) -> u32 {
+    thing.foo().await
 }
 
 struct Staff {
@@ -47,6 +61,15 @@ mod t {
         let actual = do_stuff(&mock);
         assert_eq!(actual, 88)
     }
+
+    #[tokio::test]
+    async fn test_async_method() {
+        let mut mock = AsyncThing::default();
+        mock.expect_foo().returning(|| 88);
+        let actual = do_async_stuff(&mock).await;
+        assert_eq!(actual, 88)
+    }
+
 
     #[test]
     fn test_struct(){
